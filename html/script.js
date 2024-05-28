c14f888902b059397dc3df374a2bac63dce8d0b4
스마트보안학과/202235406/권민상 <126746125+g202235406@users.noreply.github.com>
2024-05-29 01:00:18 +0900

    Add files via upload

---
+++ html/script.js
@@ -0,0 +1,7 @@
+document.getElementById('submit').addEventListener('mouseenter', function() {
+    this.classList.add('hover');
+});
+
+document.getElementById('submit').addEventListener('mouseleave', function() {
+    this.classList.remove('hover');
+});
